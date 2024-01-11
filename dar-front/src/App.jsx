056db334d5b87d232659be5e7158af375ba16dc3
Paul Charaja Torres <68834265+ch4r4j4@users.noreply.github.com>
2024-01-10 23:38:21 -0500

    initializing form

--- dar-front/src/App.jsx
+++ dar-front/src/App.jsx
@@ -1,36 +1,18 @@
 import React, { useState } from 'react';
 import './App.css'
-/*const LoginForm = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
- 
-  const handleSubmit = (e) => {
-     e.preventDefault();
-     console.log('Email:', email);
-     console.log('Password:', password);
-  };
-};*/
+
 
 
 function App() {
   return (
-    /*<form onSubmit={handleSubmit}>
-       <label>
-         Email:
-         <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-       </label>
-       <br />
-       <label>
-         Password:
-         <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-       </label>
-       <br />
-       <button type="submit">Login</button>
-    </form>*/
     <form>
-      <label htmlFor="">nombre
-      <input type="text" name="user" id="user" placeholder='mi nombre es' /></label>
-
+      {/* nombre*/}
+      <label htmlFor='nombre'>
+        Nombre
+        </label>
+        <input type="text"/>
+        <label htmlFor="contraseña">contraseña</label>
+        <input type="email"/>
     </form>
     )
 }
