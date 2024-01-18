0671465f73a9d3ddacb49181e3da2a1f7c8c231f
Paul Charaja Torres <68834265+ch4r4j4@users.noreply.github.com>
2024-01-17 21:29:33 -0500

    add reserva form

---
+++ dar-front/src/Reserva.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import {useForm} from 'react-hook-form';
+
+function Reserva(){
+    return(
+    <h1>Hola mundo</h1>
+    )
+}
+
+export default Reserva;
--- dar-front/src/main.jsx
+++ dar-front/src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Reserva from './Reserva.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  /*<React.StrictMode>
     <App />
+  </React.StrictMode>,*/
+  <React.StrictMode>
+  <Reserva />
   </React.StrictMode>,
 )
