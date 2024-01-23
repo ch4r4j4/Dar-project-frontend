83f605cfb632df6fe20409bbf408abfa4eeb1ef0
Paul Charaja Torres <68834265+ch4r4j4@users.noreply.github.com>
2024-01-22 22:49:29 -0500

    quitar handle summit

--- dar-front/src/App.jsx
+++ dar-front/src/App.jsx
@@ -8,42 +8,37 @@ import image from './assets/react.svg'
 
 /*ningun cambio por hoy di creo que ya fuimos no entiendo muchas cosas de react*/
 function App() {
-  const { register, handleSubmit, errors } = useForm();
+  /*const { register, handleSubmit, errors } = useForm();*/
 
-  const onSubmit = (data) => {
+  /*const onSubmit = (data) => {
     console.log(data);
-  };
+  };*/
 
   return (
-    <section className='firstSection'>
+   <div className='firstSection'>
     <div>
       <img src={image} alt="logo" />
     </div>
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form /*onSubmit={handleSubmit(onSubmit)}*/>
       <div className="form-group">
         <label htmlFor="email">Email</label>
         <input
           type="email"
           name="email"
-          ref={register({ required: 'Email is required' })}
-          className={`form-control ${errors.email ? 'is-invalid' : ''}`}
         />
-        {errors.email && <div className="invalid-feedback">{errors.email.message}</div>}
       </div>
       <div className="form-group">
         <label htmlFor="password">Password</label>
         <input
           type="password"
           name="password"
-          ref={register({ required: 'Password is required' })}
-          className={`form-control ${errors.password ? 'is-invalid' : ''}`}
         />
-        {errors.password && <div className="invalid-feedback">{errors.password.message}</div>}
       </div>
       <button type="submit" className="btn btn-primary">Login</button>
     </form>
-    </section>
+    </div>
     )
 }
 
 export default App
+/*por el momento nos falta configurar correctamente lo del hook-form*/
